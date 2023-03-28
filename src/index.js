2b61d908ae82b57f32b5d6ab5ae758b562efa5a1
Peter <guan_peter123@yahoo.com>
2023-03-28 16:56:59 -0700

    index.js not done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <App />
+      <h1>Hello from Root</h1>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM.render(
+  // const root = ReactDOM.createRoot(document.getElementById('root'));
+  // root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Root />
+  </React.StrictMode>, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
